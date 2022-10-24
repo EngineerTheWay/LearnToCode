ed339bfff703fe63fa2b2b2ae53c6050a069082b
Terillian <terillian@yahoo.com>
2022-10-24 19:40:49 -0400

    Some refresher projects

---
+++ intials.py
@@ -0,0 +1,11 @@
+# First name: Caleb Way
+# Learning to code and stuff
+
+print("""CCC    W    W
+C   C   W    W
+C       W    W
+C       W  W W
+C       WW  WW
+C   C   WW  WW
+ CCC    W    W
+""")
